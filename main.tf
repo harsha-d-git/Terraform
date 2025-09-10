08e77fd015c6d5bae22e56aad1d66cb5439c682d
harsha-d-git <harshadhyr@gmail.com>
2025-09-10 16:23:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ resource "aws_instance" "my-first-ec2-01" {
     ami = local.my-ami
     instance_type = local.instance_type
     key_name = var.pem-key-file
-    count = 2
+
   
     tags = {
       Name = "terraform-111"
     }
 }
+
 
