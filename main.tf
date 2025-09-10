bfc6642cd56a742a3fb0d850a19521d9c725c06b
harsha-d-git <harshadhyr@gmail.com>
2025-09-10 16:46:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ resource "aws_instance" "my-first-ec2-01" {
     ami = local.my-ami
     instance_type = local.instance_type
     key_name = var.pem-key-file
-
   
     tags = {
       Name = "terraform-111"
     }
 }
 
-
+terraform {
+  backend "s3" {
+    bucket = "terraform-aws-cicd-1997"
+    key    = "aws/terraform.tfstate"
+    region = "ap-south-1"
+  }
